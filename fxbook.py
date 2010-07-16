1b7960de8a78ccee68c08cff586636d1155bfa4c
Max Lapan <max.lapan@gmail.com>
2010-07-16 12:08:56 +0400

    Return count of history entries for account-pair.

--- fxbook.py
+++ fxbook.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
 import cgi
+import pickle
 from myfxbook import history
+from myfxbook import account
 
 print 'Content-Type: text/plain'
 print ''
@@ -14,18 +16,30 @@ if not 'pair' in cgi.FieldStorage ():
     print 'Currency pair is required'
     exit ()
 
-account = cgi.FieldStorage ()['id'].value
+count_mode = 'count' in cgi.FieldStorage ()
+
+acc_id = cgi.FieldStorage ()['id'].value
 pair = cgi.FieldStorage ()['pair'].value
 
-cache = history.HistoryDataCache (account, pair)
-
-res = cache.csv ()
-
-if res:
-    print res
+# In count_mode we just display count of orders by given currency pair and account id
+if count_mode:
+    acc = account.by_id (acc_id)
+    if not acc.pairs_map:
+        print '0'
+    else:
+        pairs_map = pickle.loads (acc.pairs_map)
+        if pair in pairs_map:
+            print pairs_map[pair]
+        else:
+            print '0'
 else:
-    hist = history.FXBookHistory (account, pair)
-    res = hist.csv ()
-    cache.set_csv (res)
-    print res
+    cache = history.HistoryDataCache (acc_id, pair)
+    res = cache.csv ()
+    if res:
+        print res
+    else:
+        hist = history.FXBookHistory (acc_id, pair)
+        res = hist.csv ()
+        cache.set_csv (res)
+        print res
 
